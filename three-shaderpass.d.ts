6993d01b9cec28bbdf4dd9743ce69a46b2d0e18d
Hou,Chunlei <omni360@qq.com>
2017-11-24 11:44:02 +0800

    update interface three.shader

--- three-shaderpass.d.ts
+++ three-shaderpass.d.ts
@@ -1,3 +1,4 @@
+
 // Type definitions for three.js (ShaderPass.js)
 // Project: https://github.com/mrdoob/three.js/blob/r68/examples/js/postprocessing/ShaderPass.js
 // Definitions by: Satoru Kimura <https://github.com/gyohk>
@@ -5,7 +6,7 @@
 
 
 declare namespace THREE {
-    export class ShaderPass {
+    export class ShaderPass extends Pass {
         constructor(shader: Shader, textureID?: string);
 
         textureID: string;
